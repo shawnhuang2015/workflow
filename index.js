2c4503ff8adf008a46e3cb9bb6ec5bca5bc694f6
shawn.huang <shawn.huang@graphsql.com>
2017-09-25 10:07:58 -0700

    add test

--- index.js
+++ index.js
@@ -1,46 +1,36 @@
-var d3 = require('d3');
-var jsdom = require('jsdom')
-  , GVIS = require('./gvis.export')
-  // , jQuery = require("jquery")(jsdom.jsdom().defaultView)
+var d3 = require('d3');;
+var jsdom = require('jsdom');
+var gvis = require('./gvis.export').GVIS;
+var fs = require('fs');
+var data = require('./data.json');
 
-  , htmlStub = '<html><head><title>qwe</title></head><body><div id="dataviz-container"></div><script src="js/d3.v3.min.js"></script></body></html>' // html file skull with a container div for the d3 dataviz
+var htmlStub = '<html><head><title>qwe</title></head><body><div id="visualization_container"></div></body></html>' // html file skull with a container div for the d3 dataviz
+
+process.argv.forEach(function(argument, i) {
+  console.log(i, argument)
+})
+
+var inputFile = process.argv[2];
+var outputFile = process.argv[3];
 
 // pass the html stub to jsDom
 jsdom.env({ 
-  features : { QuerySelector : true }, 
+  // features : { QuerySelector : true }, 
   html : htmlStub, 
   done : function(errors, window) {
     // process the html document, like if we were at client side
     // code to generate the dataviz and process the resulting html file to be added here
 
+    console.log(data.nodes.length);
 
-    var el = window.document.querySelector('#dataviz-container')
-    , body = window.document.querySelector('body')
+    var visualization = new gvis({
+      container : 'visualization_container',
+      render_type : 'svg'
+    })
 
+    visualization.backup(data).update(500);
 
-    var circleId = 'a2324'  // say, this value was dynamically retrieved from a database
-
-    // append the svg to the selector
-    d3.select(el)
-      .append('svg:svg')
-        .attr('width', 600).attr('height', 300)
-        .append('circle')
-          .attr('cx', 300).attr('cy', 150).attr('r', 30).attr('fill', '#26963c')
-          .attr('id', circleId) // we assign the circle to an Id here
-
-    // write the client-side script manipulating the circle
-    var clientScript = "d3.select('#" + circleId + "').transition().delay(1000).attr('fill', '#f9af26')"
-
-    // append the script to page's body
-    d3.select(body)
-      .append('script')
-        .html(clientScript)
-
-    var fs = require('fs')
-      , svgsrc = window.document.innerHTML
-      
-    console.log(window.document.body.innerHTML);
-    console.log(window.document.documentElement.innerHTML);
+    var svgsrc = window.document.documentElement.innerHTML;
 
     fs.writeFile('index.html', svgsrc, function(err) {
       if(err) {
