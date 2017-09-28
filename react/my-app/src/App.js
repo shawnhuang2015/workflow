417a63a1ac2cc9d421e5c80ec9f079a60b685413
shawn.huang <shawn.huang@graphsql.com>
2017-09-28 14:16:41 -0700

    done

--- react/my-app/src/App.js
+++ react/my-app/src/App.js
@@ -7,19 +7,33 @@ let HelloAwesomeness = () => {
 }
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { prop: "bar" };
+  }
+
   render() {
     let test = [];
-    for (var i=0; i<5; i++) {
-      test.push(<HelloAwesomeness />);
+    for (var i = 0; i < 5; i++) {
+      test.push(<div><HelloAwesomeness /></div>);
       test.push(<br />);
     }
-    
+
+    setTimeout(() => {
+      this.setState({
+        prop: 'new value',
+        prop2: 'new value'
+      });
+    }, 2000);
+
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <HelloAwesomeness />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to React {this.state.prop}</h2>
+          <h2>Prop2 : {this.state.prop2}</h2>
         </div>
         <p className="App-intro">
           {test}
