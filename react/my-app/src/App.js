a321611bc0e399ac79db69d7ea4dda0f03ad8a43
shawn.huang <shawn.huang@graphsql.com>
2017-09-29 18:14:32 -0700

    test life circle

--- react/my-app/src/App.js
+++ react/my-app/src/App.js
@@ -1,46 +1,136 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
 import logo from './logo.svg';
 import './App.css';
 
-let HelloAwesomeness = () => {
-  return <span>Hello Awesomeness!</span>
+let test = (a) => {
+  return '#### ' + a + '####';
+}
+let HelloAwesomeness = (props) => {
+  return <span>Hello Awesomeness {props.name}! {test('example')}  <img src={logo} className="App-logo" alt="logo" /></span>
 }
 
 class App extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { prop: "bar" };
+    this.state = { prop: 'bar', prop2: undefined };
+
+    // setTimeout(() => {
+    //   this.setState({
+    //     prop: 'new value',
+    //     prop2: 'new value'
+    //   });
+    // }, 3000);
+
+    this.value = {
+      count: 0,
+      content: '***********'
+    };
+
+    this._handleClick = this._handleClick.bind(this);
+  }
+
+  UserInfo(props) {
+    let value = this.value;
+    value.count++;
+    console.log(value);
+    return (
+      <div className="UserInfo">
+        <div className="UserInfo-name">
+          {props.user} + {value.content} + {value.count}
+        </div>
+      </div>
+    );
+  }
+
+
+  _handleClick() {
+    console.log('click', this.refs.test);
+    let mountNode = ReactDOM.findDOMNode(this.refs.test);
+    ReactDOM.unmountComponentAtNode(mountNode);
+
+    this.setState({prop: this.state.prop + '#$%^&*()_'});
   }
 
   render() {
     let test = [];
     for (var i = 0; i < 5; i++) {
-      test.push(<div><HelloAwesomeness /></div>);
-      test.push(<br />);
+      test.push(<div key={i}>{HelloAwesomeness({ name: '<>&undefined' })}></div>);
+      test.push(<br key={i + 'b'} />);
     }
 
-    setTimeout(() => {
-      this.setState({
-        prop: 'new value',
-        prop2: 'new value'
-      });
-    }, 2000);
+    const element = React.createElement(
+      'h1',
+      { className: 'greeting' },
+      'Hello, world React element!'
+    );
 
     return (
-      <div className="App">
+      <div className="App" onClick={this._handleClick}>
+        <Clock ref='test'/>
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <HelloAwesomeness />
+          <img src={logo} className="App-logo" alt="logo" />
+          <HelloAwesomeness name='React' />
+          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo" />
+          {element}
           <h2>Welcome to React {this.state.prop}</h2>
           <h2>Prop2 : {this.state.prop2}</h2>
+          {this.UserInfo({ user: this.state.prop })}
         </div>
-        <p className="App-intro">
+        <div className="App-intro">
           {test}
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        </div>
       </div>
     );
+  }
+}
+
+class Clock extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: new Date()
+    }
+
+    console.log('constructor')
+  }
+
+  componentWillMount() {
+    console.log('componentWillMount')
+    
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount')
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    console.log('componentWillUnmount');
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1> Clock : </h1>
+        <h2> It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    )
   }
 }
 
--- react/my-app/src/index.js
+++ react/my-app/src/index.js
@@ -5,4 +5,21 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root2'));
+
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <App />
+      <h2>It is {new Date().toLocaleTimeString() + ' ' + new Date().toDateString()}.</h2>
+    </div>
+  );
+  ReactDOM.render(
+    element,
+    document.getElementById('root')
+  );
+}
+
+tick();
 registerServiceWorker();
